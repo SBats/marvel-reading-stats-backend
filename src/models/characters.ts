0a172bcf642c29b322e1b1b477fbc883e802b494
Sbats <simon.bats12@gmail.com>
2016-08-07 11:22:25 +0200

    Create characters model

---
+++ src/models/characters.ts
@@ -0,0 +1,75 @@
+import DatabaseService from '../services/database';
+
+export default class Characters {
+  dbService: DatabaseService = null;
+
+  constructor() {
+    this.dbService = new DatabaseService();
+  }
+
+  getAll(): Promise<any> {
+    return this.dbService.connect()
+      .then((db: any) => {
+        return db.collection('characters').find({}, {"name": 1, "marvelId": 1, _id: 0}).sort([['name', 1]]).toArray()
+          .then((characters: any) => {
+            db.close();
+            return characters;
+          })
+          .catch((err: any) => {
+            db.close();
+            return err;
+          });
+      })
+      .catch((err: any) => err);
+  }
+
+  getStartingBy(query: string): Promise<any> {
+    return this.dbService.connect()
+      .then((db: any) => {
+        return db.collection('characters')
+          .find(
+            {"name": {$regex: `^${query}`, $options: "i"}},
+            {"name": 1, "marvelId": 1, _id: 0}
+          )
+          .sort([['name', 1]])
+          .toArray()
+          .then((characters: any) => {
+            db.close();
+            return characters;
+          })
+          .catch((err: any) => {
+            db.close();
+            return err;
+          });
+      })
+      .catch((err: any) => err);
+  }
+
+  getById(marvelId: string): Promise<any> {
+    return this.dbService.connect()
+      .then((db: any) => {
+        console.log(marvelId);
+        return db.collection('characters')
+          .find({"marvelId": parseInt(marvelId)}, {
+            _id: 0,
+            "name": 1,
+            "description": 1,
+            "urls": 1,
+            "thumbnail": 1,
+            "marvelId": 1,
+          })
+          .limit(1)
+          .next()
+          .then((character: any) => {
+            console.log(character);
+            db.close();
+            return character;
+          })
+          .catch((err: any) => {
+            db.close();
+            return err;
+          });
+      })
+      .catch((err: any) => err);
+  }
+}
