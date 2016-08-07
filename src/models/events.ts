4afec3662aba492c13854ae954c65d397fb78521
Sbats <simon.bats12@gmail.com>
2016-08-07 10:57:09 +0200

    Add getStartingBy method to events model

--- src/models/events.ts
+++ src/models/events.ts
@@ -23,6 +23,28 @@ export default class Events {
       .catch((err: any) => err);
   }
 
+  getStartingBy(query: string): Promise<any> {
+    return this.dbService.connect()
+      .then((db: any) => {
+        return db.collection('events')
+          .find(
+            {"title": {$regex: `^${query}`, $options: "i"}},
+            {"title": 1, "marvelId": 1, _id: 0}
+          )
+          .sort([['title', 1]])
+          .toArray()
+          .then((events: any) => {
+            db.close();
+            return events;
+          })
+          .catch((err: any) => {
+            db.close();
+            return err;
+          });
+      })
+      .catch((err: any) => err);
+  }
+
   getById(marvelId: string): Promise<any> {
     return this.dbService.connect()
       .then((db: any) => {
