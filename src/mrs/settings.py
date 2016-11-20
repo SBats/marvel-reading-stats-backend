9fdddc7d5312e8fa4a9a811ed1c6762c5b832ec2
SBats <simon.bats12@gmail.com>
2016-11-20 18:59:46 +0100

    Increase results per page for api

--- src/mrs/settings.py
+++ src/mrs/settings.py
@@ -145,5 +145,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 20,
 }
